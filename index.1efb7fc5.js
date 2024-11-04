ddcb9b6aeb44e6a017383b00d2beb896386511d6
Karina Koliada <koladakarina1112@gmail.com>
2024-11-04 21:55:49 +0100

    make build

---
+++ index.1efb7fc5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest("a");if(r){var a=r.getAttribute("href");t.src=a}});
+//# sourceMappingURL=index.1efb7fc5.js.map
